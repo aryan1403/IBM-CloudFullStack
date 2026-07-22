9b4906968c4dd9356e0f660d43a8c182945f66ea
aryan1403 <aaryan14032006@email.com>
2026-07-22 12:03:42 +0530

    material pushed

---
+++ js/first.js
@@ -0,0 +1,32 @@
+// greet();
+function greet() {
+    console.log("Good morning")
+}
+
+// greet();
+// function start(func) {
+//     func();
+// }
+
+// [1,2,3].forEach()
+
+// start(greet);
+
+// console.log(a);
+// var a = 5;
+
+// const c = setTimeout(() => {
+//     console.log('hey aaryan')
+// }, 1000);
+// clearTimeout(c);
+// let count = 0;
+// let d = setInterval(() => {
+//     console.log('hey aaryan 2')
+//     count++;
+//     if(count > 5) {
+//         clearInterval(d);
+//     }
+// }, 1000);
+// clearInterval(d);
+// console.log('hey aaryan 2')
+
